1e07626941b4bd155dd0d68bf3e91ed8a587ab5f
aqulu <dev@aqu.lu>
2020-01-19 10:11:40 +0900

    feat (ScanEvent): add ScanError event

--- lib/blocs/scan_event.dart
+++ lib/blocs/scan_event.dart
@@ -18,3 +18,19 @@ class Result extends ScanEvent {
   @override
   int get hashCode => code.hashCode;
 }
+
+@immutable
+class ScanError extends ScanEvent {
+  final Exception error;
+
+  const ScanError(this.error) : super();
+
+  @override
+  bool operator ==(other) {
+    return identical(this, other) ||
+        (other is ScanError && error == other.error);
+  }
+
+  @override
+  int get hashCode => error.hashCode;
+}
