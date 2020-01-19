7a06488f7c15c53df03696cd282a126332195fab
aqulu <dev@aqu.lu>
2020-01-19 15:01:42 +0900

    feat (ScanScreen): add result display

--- lib/ui/scan_screen.dart
+++ lib/ui/scan_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_qr_reader/flutter_qr_reader.dart';
 import 'package:qr_scanner/blocs/scan_bloc.dart';
 import 'package:qr_scanner/blocs/scan_event.dart';
+import 'package:qr_scanner/blocs/scan_state.dart';
 
 class ScanScreen extends StatelessWidget {
   @override
@@ -13,26 +14,47 @@ class ScanScreen extends StatelessWidget {
       );
 }
 
-class _ScanScreen extends StatefulWidget {
-  @override
-  __ScanScreenState createState() => __ScanScreenState();
-}
-
-class __ScanScreenState extends State<_ScanScreen> {
-  void _onQrReaderInitialized(QrReaderViewController controller) {
-    controller.startCamera((code, _) {
-      BlocProvider.of<ScanBloc>(context).add(Result(code));
-    });
-  }
-
+class _ScanScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: LayoutBuilder(
-          builder: (context, constraints) => QrReaderView(
-            height: constraints.maxHeight,
-            width: constraints.maxWidth,
-            callback: _onQrReaderInitialized,
-          ),
+        body: Stack(
+          children: <Widget>[
+            LayoutBuilder(
+              builder: (context, constraints) => QrReaderView(
+                height: constraints.maxHeight,
+                width: constraints.maxWidth,
+                callback: (QrReaderViewController controller) {
+                  controller.startCamera((code, _) {
+                    BlocProvider.of<ScanBloc>(context).add(Result(code));
+                  });
+                },
+              ),
+            ),
+            SizedBox.expand(
+              child: DraggableScrollableSheet(
+                minChildSize: 0.25,
+                initialChildSize: 0.25,
+                maxChildSize: 0.75,
+                builder: (_, ScrollController scrollController) => Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white60,
+                    borderRadius: const BorderRadius.vertical(
+                      top: const Radius.circular(8.0),
+                    ),
+                  ),
+                  child: BlocBuilder<ScanBloc, ScanState>(
+                    builder: (_, ScanState state) => ListView.builder(
+                      controller: scrollController,
+                      itemCount: state.results.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text(state.results[index].code),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       );
 }
