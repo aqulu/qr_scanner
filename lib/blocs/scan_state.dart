8f75cf97b83e47ff3f1eb71e7db7db8235de30fc
aqulu <dev@aqu.lu>
2020-01-19 10:03:38 +0900

    feat (ScanState): add error to ScanState

--- lib/blocs/scan_state.dart
+++ lib/blocs/scan_state.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:qr_scanner/models/qr_code.dart';
 
@@ -6,14 +7,29 @@ import 'package:qr_scanner/models/qr_code.dart';
 class ScanState {
   final List<QrCode> results;
 
-  const ScanState(this.results);
+  final Option<Exception> error;
+
+  ScanState(List<QrCode> results, {Exception error})
+      : this._(results ?? [], optionOf(error));
+
+  const ScanState._(this.results, this.error)
+      : assert(results != null && error != null);
+
+  ///
+  /// creates a new [ScanState] using the provided non-null parameters
+  /// and copies the remaining values from the current ScanState
+  ///
+  ScanState copy({List<QrCode> results, Option<Exception> error}) =>
+      ScanState._(results ?? this.results, error ?? this.error);
 
   @override
   bool operator ==(other) {
     return identical(this, other) ||
-        (other is ScanState && listEquals(results, other.results));
+        (other is ScanState &&
+            listEquals(results, other.results) &&
+            error == other.error);
   }
 
   @override
-  int get hashCode => results.hashCode;
+  int get hashCode => results.hashCode ^ error.hashCode;
 }
