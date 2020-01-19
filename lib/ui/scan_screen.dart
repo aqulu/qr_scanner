b0cb26db99e390e06af3e5d69075c8fd21c6d8d2
aqulu <dev@aqu.lu>
2020-01-19 16:22:16 +0900

    refactor (ScanScreen): reorganize scaffold contents

--- lib/ui/scan_screen.dart
+++ lib/ui/scan_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_qr_reader/flutter_qr_reader.dart';
 import 'package:qr_scanner/blocs/scan_bloc.dart';
 import 'package:qr_scanner/blocs/scan_event.dart';
 import 'package:qr_scanner/blocs/scan_state.dart';
+import 'package:qr_scanner/models/qr_code.dart';
 
 class ScanScreen extends StatelessWidget {
   @override
@@ -19,6 +20,7 @@ class _ScanScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
         body: Stack(
           children: <Widget>[
+            // wrap into LayoutBuilder to get concrete width / height parameters for QrReaderView
             LayoutBuilder(
               builder: (context, constraints) => QrReaderView(
                 height: constraints.maxHeight,
@@ -30,31 +32,35 @@ class _ScanScreen extends StatelessWidget {
                 },
               ),
             ),
-            SizedBox.expand(
-              child: DraggableScrollableSheet(
-                minChildSize: 0.25,
-                initialChildSize: 0.25,
-                maxChildSize: 0.75,
-                builder: (_, ScrollController scrollController) => Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white60,
-                    borderRadius: const BorderRadius.vertical(
-                      top: const Radius.circular(8.0),
-                    ),
-                  ),
-                  child: BlocBuilder<ScanBloc, ScanState>(
-                    builder: (_, ScanState state) => ListView.builder(
-                      controller: scrollController,
-                      itemCount: state.results.length,
-                      itemBuilder: (context, index) => ListTile(
-                        title: Text(state.results[index].code),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            SizedBox.expand(child: _bottomSheet),
           ],
         ),
+      );
+
+  Widget get _bottomSheet => DraggableScrollableSheet(
+        minChildSize: 0.25,
+        initialChildSize: 0.25,
+        maxChildSize: 0.75,
+        builder: (_, ScrollController scrollController) => Container(
+          decoration: const BoxDecoration(
+            color: Colors.white60,
+            borderRadius: const BorderRadius.vertical(
+              top: const Radius.circular(8.0),
+            ),
+          ),
+          child: BlocBuilder<ScanBloc, ScanState>(
+            builder: (_, ScanState state) => ListView.builder(
+              controller: scrollController,
+              itemCount: state.results.length,
+              itemBuilder: (context, index) => _buildResultRow(
+                state.results[index],
+              ),
+            ),
+          ),
+        ),
+      );
+
+  ListTile _buildResultRow(QrCode code) => ListTile(
+        title: Text(code.code),
       );
 }
