a28af1bc0b560449e26e0f65696513c03016adab
aqulu <dev@aqu.lu>
2020-01-19 10:14:26 +0900

    feat (ScanBloc): implement ScanBloc

---
+++ lib/blocs/scan_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qr_scanner/blocs/scan_event.dart';
+import 'package:qr_scanner/blocs/scan_state.dart';
+import 'package:qr_scanner/models/qr_code.dart';
+
+class ScanBloc extends Bloc<ScanEvent, ScanState> {
+  @override
+  ScanState get initialState => ScanState([]);
+
+  @override
+  Stream<ScanState> mapEventToState(ScanEvent event) async* {
+    if (event is Result) {
+      final currentState = state;
+      yield currentState.copy(
+        results: [QrCode(event.code), ...currentState.results],
+        error: None(),
+      );
+    }
+
+    if (event is ScanError) {
+      yield state.copy(error: Some(event.error));
+    }
+  }
+}
